8b83bdef63e1388ee427cdc2363d7d7571a7f482
Rinshika Dubey <125173389+RINSHIKA@users.noreply.github.com>
2023-09-10 17:59:35 +0000

    second config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 14453
+let account_email = "rinshiuka@gmail.com"
+var account_pass = "2345"
+account_city = "Ranchi"
+account_email = "hc.com"
+account_pass = "6466"
+account_city = "bangluru"
+// console.log(account_email);
+// console.table([accountId, account_email , account_city, account_pass])
+// var not use in javascript because of scope and functional scope
+// ***************************DATA TYPES**************************
+// number = 2 to power 53
+// bigint
+// string = " "
+// boolean = true/ false
+// undefined 
+// symbol = unique value
+// type od null is object
+let c;
+// console.log(typeof c);
